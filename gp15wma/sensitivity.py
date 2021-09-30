4cf0c4b78de486b52a64b44180577083aca4d2c8
Av Shrikumar <avanti.shrikumar@gmail.com>
2021-09-30 13:26:12 -0700

    cleaned up the skeleton export code

--- gp15wma/sensitivity.py
+++ gp15wma/sensitivity.py
@@ -1,5 +1,6 @@
 from __future__ import division, print_function
 from pyompa import OMPAProblem
+from pyompa.core import ExportToCsvMixin
 from collections import namedtuple
 
 
@@ -30,3 +31,77 @@ class BaseSensitivityAnalysis(object):
                   **export_settings)
           ompa_solns.append(ompa_soln)
       return ompa_solns
+
+
+#Creating ExportToCsvMixin objects (skeleton soln objects)
+# for the mean and standard deviation
+# so I can reuse some of the plotting and export code
+def get_mean_and_stdev_skeletons(ompa_solns):
+
+    mean_endmember_fractions =\
+      np.mean([x.endmember_fractions for x in ompa_solns], axis=0)
+    std_endmember_fractions = np.std([
+      x.endmember_fractions for x in ompa_solns], axis=0)
+
+    mean_param_residuals =\
+      np.mean([x.param_residuals for x in ompa_solns], axis=0)
+    std_param_residuals = np.std([
+      x.param_residuals for x in ompa_solns], axis=0)
+    
+    mean_groupname_to_totalconvertedvariable = OrderedDict()
+    std_groupname_to_totalconvertedvariable = OrderedDict()
+
+    mean_groupname_to_effectiveconversionratios = OrderedDict()
+    std_groupname_to_effectiveconversionratios = OrderedDict()
+    
+    groupnames = ompa_solns[0].groupname_to_totalconvertedvariable.keys()
+    for groupname in groupnames:
+        mean_groupname_to_totalconvertedvariable[groupname] =\
+          np.mean([x.groupname_to_totalconvertedvariable[groupname]
+                   for x in ompa_solns], axis=0)
+        std_groupname_to_totalconvertedvariable[groupname] =\
+          np.std([x.groupname_to_totalconvertedvariable[groupname]
+                   for x in ompa_solns], axis=0)
+        
+        convratiokeys =\
+          ompa_solns[0].groupname_to_effectiveconversionratios[groupname].keys()
+        mean_groupname_to_effectiveconversionratios[groupname] = OrderedDict()
+        std_groupname_to_effectiveconversionratios[groupname] = OrderedDict()
+
+        for convratiokey in convratiokeys:
+          mean_groupname_to_effectiveconversionratios[
+             groupname][convratiokey] = np.mean([
+              x.groupname_to_effectiveconversionratios[groupname][convratiokey]
+              for x in ompa_solns], axis=0)
+          std_groupname_to_effectiveconversionratios[
+             groupname][convratiokey] = np.std([
+              x.groupname_to_effectiveconversionratios[groupname][convratiokey]
+              for x in ompa_solns], axis=0)
+             
+    endmember_names = list(                                                    
+            ompa_solns[0].endmember_df[ompa_solns[0].endmember_name_column])
+    obs_df = ompa_solns[0].obs_df
+
+    mean_ompa_skeleton = ExportToCsvMixin(
+        param_names=ompa_solns[0].param_names,
+        endmember_names=endmember_names,
+        obs_df=obs_df,
+        endmember_fractions=mean_endmember_fractions,
+        param_residuals=mean_param_residuals,
+        groupname_to_effectiveconversionratios=
+          mean_groupname_to_effectiveconversionratios,
+        groupname_to_totalconvertedvariable=
+          mean_groupname_to_totalconvertedvariable)
+    
+    std_ompa_skeleton = ExportToCsvMixin(
+        param_names=ompa_solns[0].param_names,
+        endmember_names=endmember_names,
+        obs_df=obs_df,
+        endmember_fractions=std_endmember_fractions,
+        param_residuals=std_param_residuals,
+        groupname_to_effectiveconversionratios=
+          std_groupname_to_effectiveconversionratios,
+        groupname_to_totalconvertedvariable=
+          std_groupname_to_totalconvertedvariable)
+
+    return (mean_ompa_skeleton, std_ompa_skeleton)
