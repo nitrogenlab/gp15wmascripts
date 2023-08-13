fdce3348967f7dc02a95f212b53995e5dae961e1
rml54 <45828236+rml54@users.noreply.github.com>
2023-08-13 16:07:32 -0400

    Update funcdump.py

--- GP02wma/funcdump.py
+++ GP02wma/funcdump.py
@@ -14,7 +14,7 @@ def download_GP02_data():
    #os.system("wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1Gla6o_YihOCfU5pWGLhFvL-TKm_0aXfQ' -O names_added_GP02OMPA_33RR20180918_only_gs_rosette_clean1_hy1.csv") 
     #os.system("wget 'http://optserv1.whoi.edu/jgofsopt/80/128.12.123.170/GP02_Bottle_Leg1.mat' -O GP02_Bottle_Leg1.mat")
     #os.system("wget 'http://optserv1.whoi.edu/jgofsopt/80/128.12.123.170/GP02_Bottle_Leg2.mat' -O GP02_Bottle_Leg2.mat")
-    os.system("wget 'https://github.com/nitrogenlab/gp15wmascripts/blob/e91f38333636b0f9fa5a97b49c351bc3e4f54028/GP02wma/bottleGP02_IDP2021_v2_GEOTRACES_Seawater_Discrete_Sample_Data_v2_wlG854xv.csv' -O bottleGP02_IDP2021_v2_GEOTRACES_Seawater_Discrete_Sample_Data_v2_wlG854xv.csv")
+    os.system("wget 'https://github.com/nitrogenlab/GP02wmascripts/blob/e91f38333636b0f9fa5a97b49c351bc3e4f54028/GP02wma/bottleGP02_IDP2021_v2_GEOTRACES_Seawater_Discrete_Sample_Data_v2_wlG854xv.csv' -O bottleGP02_IDP2021_v2_GEOTRACES_Seawater_Discrete_Sample_Data_v2_wlG854xv.csv")
 
 def augment_df_with_PO_NO_SiO(df):  
     #remineralization ratios
@@ -28,131 +28,86 @@ def augment_df_with_PO_NO_SiO(df):
 
 
 def download_and_load_GP02_data(station_to_tc_cutoffs_url,
-                                cutoffs_file_name="station_to_tc_cutoffs.json"):
+                                cutoffs_file_name="GP02_station_to_tc_cutoffs.json"):
     download_GP02_data()
     return load_GP02_data(station_to_tc_cutoffs_url=station_to_tc_cutoffs_url,
                           cutoffs_file_name=cutoffs_file_name)
 
+def download_and_load_GP02_data(station_to_tc_cutoffs_url="https://github.com/nitrogenlab/GP15_watermassanalysis/blob/0a64ed0faca01701cf6c84d09365abc706594e2c/GP02_station_to_tc_cutoffs.json",
+                                cutoffs_file_name="GP02_station_to_tc_cutoffs.json"):
+    download_GP02_data()
+    return load_GP02_data(station_to_tc_cutoffs_url=station_to_tc_cutoffs_url,
+                          cutoffs_file_name=cutoffs_file_nam)
+def GP02_load_data():
+    colnames_subset = ['G2cruise', 'G2station', "G2latitude", 'G2longitude', 'G2year', 'G2depth',
+                   'G2pressure', 'G2temperature', 'G2salinity',
+                   'G2salinityf','G2oxygen','G2oxygenf','G2silicate',
+                   'G2silicatef',  'G2nitrate', 'G2nitratef', 
+                   'G2phosphate', 'G2phosphatef','G2sigma0']
 
-def GP02_load_mat_data():
-    leg1 = scipy.io.loadmat('GP02_Bottle_Leg1.mat')
-    leg2 = scipy.io.loadmat('GP02_Bottle_Leg2.mat')
 
-    header_mapping = {
-        'bottle flag': ('BTLNBR_FLAG_W', 'BTLNBR_FLAG_W'),
-        'CTD salinity flag': ('CTDSAL_FLAG_W', 'CTDSAL_FLAG_W'),
-        #"bottle salinity flag": ('Flag_SALINITY_D_CONC_BOTTLE_tcj2lg',
-        #                         'Flag_SALINITY_D_CONC_BOTTLE_zva7jm'),
-        "bottle oxygen flag": ('Flag_OXYGEN_D_CONC_BOTTLE_qizf9x',
-                               'Flag_OXYGEN_D_CONC_BOTTLE_n41f8b'),
-        "silicate flag": ('Flag_SILICATE_D_CONC_BOTTLE_l9fh07',
-                          'Flag_SILICATE_D_CONC_BOTTLE_3fot83'),
-        "nitrate flag": ('Flag_NITRATE_D_CONC_BOTTLE_xhgtuv',
-                         'Flag_NITRATE_D_CONC_BOTTLE_bugat8'),
-        "phosphate flag": ('Flag_PHOSPHATE_D_CONC_BOTTLE_lof4ap',
-                           'Flag_PHOSPHATE_D_CONC_BOTTLE_d0rgav'),
-        "CTD pressure": ('CTDPRS', 'CTDPRS'),
-        "CTD temperature" : ('CTDTMP', 'CTDTMP'),
-        "practical_salinity" : ('CTDSAL', 'CTDSAL'), #CTD practical salinity
-        "lat" : ('LATITUDE', 'LATITUDE'),
-        "lon": ('LONGITUDE', 'LONGITUDE'),
-        "stnnbr": ('STNNBR', 'STNNBR'),
-        "geotrc_ID": ('GEOTRC_SAMPNO', 'GEOTRC_SAMPNO'),
-        "bottom depth": ('DEPTH', 'DEPTH'),
-        "oxygen": ('OXYGEN_D_CONC_BOTTLE_qizf9x',
-                   'OXYGEN_D_CONC_BOTTLE_n41f8b'),
-        "silicate": ('SILICATE_D_CONC_BOTTLE_l9fh07',
-                     'SILICATE_D_CONC_BOTTLE_3fot83'),
-        "nitrate": ('NITRATE_D_CONC_BOTTLE_xhgtuv',
-                    'NITRATE_D_CONC_BOTTLE_bugat8'),
-        'phosphate': ('PHOSPHATE_D_CONC_BOTTLE_lof4ap',
-                      'PHOSPHATE_D_CONC_BOTTLE_d0rgav')
-    }
+    GP02_df = pd.read_csv("GLODAPv2.2021_Pacific_Ocean.csv", na_values = -9999)[colnames_subset]
 
-    dict_for_data_frame = OrderedDict()
+    GP02_columns =['cruise', 'stnnbr',"lat", 'lon', 'year', 'depth',
+             'CTD pressure', 'temperature','salinity', "salinity flag", 
+             'oxygen', "oxygen flag", 'silicate', "silicate flag", 'nitrate', 
+             "nitrate flag", 'phosphate', "phosphate flag", 'sigma0'] 
+    GP02_df.columns= GP02_columns
 
-    def convert_if_string(arr, legname):
-        numpy_arr = np.array(arr.squeeze())
-        if  (legname == "STNNBR" or legname == "GEOTRC_SAMPNO"):
-            return numpy_arr
-        elif (str(numpy_arr.dtype) == "<U4" or str(numpy_arr.dtype) == "<U5" 
-          or str(numpy_arr.dtype) == "<U6" or str(numpy_arr.dtype) == "<U7" 
-          or str(numpy_arr.dtype) == "<U8" or str(numpy_arr.dtype) == "<U9" 
-          or str(numpy_arr.dtype) == "<U11" or str(numpy_arr.dtype) == "<U12"): 
-            return np.array([(float(x) if x != 'nd' else np.nan)
-                             for x in numpy_arr])
-        else:
-            return numpy_arr
+    absolute_salinity = np.array((gsw.SA_from_SP(SP=GP02_df["salinity"],
+                                   p=GP02_df["CTD pressure"],
+                                   lon=GP02_df["lon"],
+                                   lat=GP02_df["lat"])))
+    GP02_df["absolute_salinity"] = absolute_salinity
 
-    for (new_header_name,
-         (leg1_name, leg2_name)) in header_mapping.items():
-        print(new_header_name, leg1_name, leg2_name)
-        leg1_arr = convert_if_string(leg1[leg1_name], leg1_name)
-        leg2_arr = convert_if_string(leg2[leg2_name], leg2_name)
-        print(leg1_arr.dtype)
-        if (str(leg1_arr.dtype)=='uint8'
-             or str(leg1_arr.dtype)=='float64'):
-            print("leg1 nans", np.sum(np.isnan(leg1_arr)))
-            print("leg2 nans", np.sum(np.isnan(leg2_arr)))
-        else:
-            print('leg1 and leg2 arrays are strings.')
-        dict_for_data_frame[new_header_name] = np.concatenate(
-            [leg1_arr, leg2_arr])
+    conservative_temp = gsw.CT_from_t(SA=absolute_salinity,
+                                  t=np.array(GP02_df["temperature"]),
+                                  p=np.array(GP02_df["CTD pressure"]))
+    GP02_df["conservative_temp"] = conservative_temp
 
-    GP02_df = pd.DataFrame(dict_for_data_frame)
+    potential_temp = gsw.pt_from_CT(SA=absolute_salinity,
+                                CT=conservative_temp)
+    GP02_df["potential_temp"] = potential_temp
+
+    sig0 = gsw.rho(SA=np.array(absolute_salinity), CT=np.array(conservative_temp), p=0) - 1000
+    GP02_df["sigma0"] = sig0
+
+    z = gsw.z_from_p(p=np.array(GP02_df["CTD pressure"]), lat=np.array(GP02_df["lat"]))
+    depth = -z #https://github.com/TEOS-10/python-gsw/blob/7d6ebe8114c5d8b4a64268d36100a70e226afaf6/gsw/gibbs/conversions.py#L577
+    GP02_df["Depth"] = depth
+    
+    #merge cruises 276 and 277 as one 
+    GP02_df = GP02_df.replace({'cruise': 277}, 276)
+
+    print("Rows:",len(GP02_df))
 
     return GP02_df
 
 
-def load_GP02_data_unsplit():
-    GP02_df = GP02_load_mat_data()
-    #remove bad data
-    for flag_type in ["bottle flag", "CTD salinity flag", "bottle oxygen flag",
+def load_GP02_data_split(cruise_number):
+    GP02_df = GP02_load_data()
+    #filter out bad data
+    for flag_type in ["salinity flag", "oxygen flag",
                       "silicate flag", "nitrate flag", "phosphate flag"]:
-        GP02_df = GP02_df[GP02_df[flag_type] <= 3]
+        GP02_df = GP02_df[GP02_df[flag_type] > 0]
+    print("no. of rows with flag above zero:",len(GP02_df))
+    #make into df
     GP02_df = pd.DataFrame(GP02_df)
-
-    #add PO and NO to data frame
-    augment_df_with_PO_NO_SiO(GP02_df)
-
-    absolute_salinity = gsw.SA_from_SP(
-        SP=np.array(GP02_df["practical_salinity"]),
-        p=np.array(GP02_df["CTD pressure"]),
-        lon=np.array(GP02_df["lon"]),
-        lat=np.array(GP02_df["lat"]))
-    GP02_df["absolute_salinity"] = absolute_salinity
-
-    conservative_temp = gsw.CT_from_t(SA=absolute_salinity,
-                                      t=np.array(GP02_df["CTD temperature"]),
-                                      p=np.array(GP02_df["CTD pressure"]))
-    GP02_df["conservative_temp"] = conservative_temp
-
-    potential_temp = gsw.pt_from_CT(SA=absolute_salinity,
-                                    CT=conservative_temp)
-    GP02_df["potential_temp"] = potential_temp
-
-    sig0 = gsw.rho(SA=absolute_salinity, CT=conservative_temp, p=0) - 1000
-    GP02_df["sigma0"] = sig0
-
-    z = gsw.z_from_p(p=np.array(GP02_df["CTD pressure"]),
-                     lat=np.array(GP02_df["lat"]))
-    depth = -z #https://github.com/TEOS-10/python-gsw/blob/7d6ebe8114c5d8b4a64268d36100a70e226afaf6/gsw/gibbs/conversions.py#L577
-    GP02_df["Depth"] = depth
-
-    spic0 = gsw.spiciness0(SA=absolute_salinity, CT=conservative_temp)
-    GP02_df["spiciness"] = spic0
-
-    print("Rows in GP02 datafile:",len(GP02_df))
+    #remove rows with missing data
     GP02_df = GP02_df.dropna()
-    print("Rows without NA values:",len(GP02_df))
-
+    print("no. of rows with NaN:",len(GP02_df))
+    #remove ros west of -140
+    GP02_df = GP02_df[GP02_df['lon']<-140]
+    print("no. of rows east of -140:",len(GP02_df))
+    GP02_df = GP02_df[GP02_df['cruise'] == cruise_number]
+    print("no. of sample for cruise",cruise_number, ":",len(GP02_df))
     return GP02_df
 
 
 def load_GP02_data(station_to_tc_cutoffs_url,
-                   cutoffs_file_name):
+                   cutoffs_file_name, cruise_number):
 
-    GP02_df = load_GP02_data_unsplit()
+    GP02_df = load_GP02_data_split(cruise_number)
 
     download_file(url=station_to_tc_cutoffs_url, file_name=cutoffs_file_name)
     station_to_tcstartend = json.loads(open(cutoffs_file_name).read())
@@ -181,7 +136,6 @@ def load_interanddeep_endmember_df(
         df_file_name="GP02_intermediateanddeep.csv"):
     download_file(url=df_url, file_name=df_file_name)
     endmember_df = pd.read_csv(df_file_name)
-    endmember_df = augment_df_with_PO_NO_SiO(endmember_df)
     return endmember_df
 
 
